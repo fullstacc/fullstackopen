51c5bd934dd513ed4b2389f5436df0ab26954b8e
sulfuroussquares <bbetancova@gmail.com>
2022-01-31 14:12:38 -0800

    made it to 1.12

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+const Statisticsline = ({ text, value }) => (
+  <div>
+    <p>
+      {text} : {value}
+    </p>
+  </div>
+);
 
+const Button = ({ handleClick, text }) => (
+  <div>
+    <button onClick={handleClick}>{text}</button>
+  </div>
+);
+
+const Statistics = (props) => {
+  const { good, bad, neutral } = props;
+  // const good = props.good;
+  // const bad = props.bad;
+  // const neutral = props.neutral;
   const allScores = good + neutral + bad;
-  const avgScore = () => {
-    if (good === 0 && neutral === 0 && bad === 0) {
-      return 0;
-    } else {
-      let total = good + neutral + bad;
-      let average = (good + bad) / total;
-      return average;
-    }
-  };
 
   const positiveFeedback = () => {
     if (good === 0 && neutral === 0 && bad === 0) {
@@ -27,6 +31,66 @@ const App = () => {
     }
   };
 
+  const avgScore = () => {
+    if (good === 0 && neutral === 0 && bad === 0) {
+      return 0;
+    } else {
+      let total = good + neutral + bad;
+      let average = (good + bad) / total;
+      return average;
+    }
+  };
+  if (good || bad || neutral) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td> good </td>
+              <td>{good}</td>
+            </tr>
+            <tr>
+              <td> neutral </td>
+              <td> {neutral} </td>
+            </tr>
+            <tr>
+              <td> bad </td>
+              <td> {bad} </td>
+            </tr>
+            <tr>
+              <td> scores </td>
+              <td> {allScores} </td>
+            </tr>
+            <tr>
+              <td> average </td>
+              <td> {avgScore()} </td>
+            </tr>
+            <tr>
+              <td> positive </td>
+              <td> {positiveFeedback()} </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+}; // end Statistics
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // TODO: make a props object containing everything needed to run statistics
+
   const addGood = () => setGood(good + 1);
   const addNeutral = () => setNeutral(neutral + 1);
   const addBad = () => setBad(bad + 1);
@@ -34,22 +98,11 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={addGood} text="good">
-        good
-      </button>
-      <button onClick={addNeutral} text="neutral">
-        neutral
-      </button>
-      <button onClick={addBad} text="bad">
-        bad
-      </button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {allScores}</p>
-      <p>average {avgScore()}</p>
-      <p>positive {positiveFeedback()}</p>
+      <Button handleClick={addGood} text="good" />
+      <Button handleClick={addNeutral} text="neutral" />
+      <Button handleClick={addBad} text="bad" />
+
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
@@ -62,4 +115,15 @@ Expand your application so that it shows more statistics about the
 gathered feedback: the total number of collected feedback,
 the average score (good: 1, neutral: 0, bad: -1) and the percentage
 of positive feedback.
+*/
+
+/*
+1.8: unicafe step3
+Refactor your application so that displaying the statistics is extracted into its own Statistics component.
+The state of the application should remain in the App root component.
+*/
+
+/*
+1.10: Button for defining the buttons used for submitting feedback
+StatisticLine for displaying a single statistic, e.g. the average score.
 */
