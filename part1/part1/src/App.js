07a313e69253941809ebd9b7c255923982d28929
sulfuroussquares <bbetancova@gmail.com>
2022-04-26 08:30:04 -0600

    built 1.1: course information

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,7 +1,78 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+const Header = (props) => {
+  return (
+    <div>
+      <h1> {props.title} </h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.title} {props.numOfExercises}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.totalExercises}</p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.partsAndExercises[0]} {props.partsAndExercises[1]}
+      </p>
+      <p>
+        {props.partsAndExercises[2]} {props.partsAndExercises[3]}
+      </p>
+      <p>
+        {props.partsAndExercises[4]} {props.partsAndExercises[5]}
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  // Content renders the parts and their number of exercises
+  // Total renders the total number of exercises
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content
+        partsAndExercises={[
+          part1,
+          exercises1,
+          part2,
+          exercises2,
+          part3,
+          exercises3,
+        ]}
+      />
+
+      <Total totalExercises={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
 
 export default App;
+
+/* 1.1 Unfortunately, the entire application is in the same component. 
+Refactor the code so that it consists of three new components: Header, Content, and Total.
+All data still resides in the App component, which passes the necessary data to each component using props */
