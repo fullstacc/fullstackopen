e0e8427fe9596df4442102575b3264709be00ca9
sulfuroussquares <bbetancova@gmail.com>
2022-05-03 20:56:27 -0600

    completed 2.1

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Course = ({ course }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header name={course.name} />
+      <div>
+        <Content parts={course.parts} />
+      </div>
     </div>
   );
-}
+};
 
+const Header = ({ name }) => (
+  <div>
+    <h1>{name}</h1>
+  </div>
+);
+
+// content is used to return parts
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <div key={part.id}>
+          {part.name} {part.exercises}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Part = (props) => {};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
 export default App;
+
+// 2.1 Define a component responsible for formatting a single course called Course.
+// App -> Course
+// Header, Content, [Parts]
