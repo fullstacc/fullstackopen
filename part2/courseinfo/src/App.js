149675f9c2b0c4c7a5248b5a31229472c94e1ca1
sulfuroussquares <bbetancova@gmail.com>
2022-05-04 06:52:59 -0600

    completed 2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -2,9 +2,21 @@ const Course = ({ course }) => {
   return (
     <div>
       <Header name={course.name} />
-      <div>
-        <Content parts={course.parts} />
-      </div>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  console.log('total of', parts);
+  const totalExercises = parts.reduce((sum, exercise) => {
+    return (sum += exercise.exercises);
+  }, 0);
+
+  return (
+    <div>
+      <b>total of {totalExercises} exercises</b>
     </div>
   );
 };
@@ -28,7 +40,6 @@ const Content = ({ parts }) => {
 
 // parts are the lowest level of components
 const Part = ({ part }) => {
-  console.log('props from part component', part);
   return (
     <div key={part.id}>
       {' '}
@@ -38,32 +49,67 @@ const Part = ({ part }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return courses.map((course) => {
+    return <Course course={course} />;
+  });
 };
+
 export default App;
 
 // 2.1 Define a component responsible for formatting a single course called Course.
 // App -> Course
 // Header, Content, [Parts]
+
+// 2.2: Course information step7
+// Show also the sum of the exercises of the course.
+
+// 2.3*: Course information step8
+// If you haven't done so already, calculate the sum of exercises with the array method reduce.
+
+// 2.4: Course information step9
+// Let's extend our application to allow for an arbitrary number of courses
