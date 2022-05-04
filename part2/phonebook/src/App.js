ada57b21154c98ad52e57a8cf4e51b28e6ab6080
sulfuroussquares <bbetancova@gmail.com>
2022-05-04 10:21:53 -0600

    completed 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,6 +8,8 @@ const App = () => {
 
   // The newName state is meant for controlling the form input element.
   const [newName, setNewName] = useState('enter name here')
+  const [newNumber, setNewNumber] = useState('enter number here')
+
 
 
   // function for adding contacts to the persons state
@@ -16,21 +18,36 @@ const App = () => {
     event.preventDefault()
     const contactObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     }
 
-    // use the setPersons() function to modify persons state
+    // check persons array for presence of duplicate name
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+    }
+    else {
+      // use the setPersons() function to modify persons state
     // use concat vice .push() because we don't mutate state directly in React
     setPersons(persons.concat(contactObject))
     setNewName('')
 
+    }
   }
 
 
   // function for updating input field state
-  const handleinputContact = (event) => {
+  const handleInputName = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+    
+  }
+
+  const handleInputNumber = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+    
   }
 
 
@@ -40,17 +57,22 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addContact}>
         <div>
-          name: <input value={newName} onChange={handleinputContact}/>
+          name: <input value={newName} onChange={handleInputName}/>
         </div>
+        <div>number: <input value={newNumber} onChange={handleInputNumber} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
+        <ul>
         {persons.map((person) => {
-          return <div>{person.name}</div>
+          return <li key={person.id}> {person.name} {person.number}
+          </li>
         })}
+        </ul>
+        
       </div>
     </div>
   )
@@ -59,4 +81,12 @@ const App = () => {
 export default App
 
 // 2.6: As a user, I can add names to the phonebook and have them added to the list of "numbers"
-// 2.7
+// 2.7: Prevent the user from being able to add names that already exist in the phonebook.
+// 2.8: Expand your application by allowing users to add phone numbers to the phone book. 
+
+// 2.9: Implement a search field that can be used to filter the list of people by name
+// 2.10: If you have implemented your application in a single component, refactor it by extracting suitable parts into new components.
+// Maintain the application's state and all event handlers in the App root component.
+
+
+
