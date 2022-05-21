6c57e435b68c948637bdc754f1fabf4b618a2360
sulfuroussquares <bbetancova@gmail.com>
2022-05-21 08:29:42 -0600

    installed json-server & completed 2.15: can now successfully POST new contacts to server

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,20 +1,25 @@
-const PersonForm = ({addContact, handleInputName, newNumber, newName, handleInputNumber}) => {
+const PersonForm = ({
+  addContact,
+  handleInputName,
+  newNumber,
+  newName,
+  handleInputNumber,
+}) => {
+  return (
+    <form onSubmit={addContact}>
+      <h2> Add Entry </h2>
+      <div>
+        name: <input value={newName} onChange={handleInputName} />
+      </div>
 
+      <div>
+        number: <input value={newNumber} onChange={handleInputNumber} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
 
-    return (
-        <form onSubmit={() => addContact}>
-        <h2> Add Entry </h2>
-        <div>
-          name: <input value={newName} onChange={handleInputName}/>
-        </div>
-        
-        <div>number: <input value={newNumber} onChange={handleInputNumber} /></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-    )
-
-}
-
-export default PersonForm
+export default PersonForm;
