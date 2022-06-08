28aca50de13b3a065b1b072a476786b7078cb640
sulfuroussquares <bbetancova@gmail.com>
2022-06-08 07:06:28 -0600

    added updated interface with backend app [see backend-testing repo]

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 // const baseUrl = 'http://localhost:3001/persons';
 const baseUrl = '/api/persons';
 
+//mongo schema
+// const personSchema = new mongoose.Schema({})
+
 // method for retrieving persons
 const getAll = () => {
   return axios.get(baseUrl);
